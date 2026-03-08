41795a84755dee1cb92ea327ee5f22b023bc9157
ST10452102 <164493462+Sethusa-Keamogetswe@users.noreply.github.com>
2026-03-08 14:17:56 +0200

    FIRST COMMIT
    EMPLOYEE

---
+++ Employee.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Employee
+{
+    private string name;
+    private string surname;
+    private string departmentName;
+    private double sales;
+
+    public Employee()
+    {
+        name = "";
+        surname = "";
+        departmentName = "";
+        sales = 0.0;
+
+    }
+
+    public void setName(string n)
+    {
+        if (!string.IsNullOrEmpty(n))
+        {
+            name = char.ToUpper(n[0]) + n.Substring(1).ToLower();
+        }
+    }
+
+    public void setSurname(string s)
+    {
+        surname = s.ToUpper();
+
+    }
+
+    public void setDepartmentName(char d)
+    {
+        d = Char.ToUpper(d);
+
+        if (d == 'E')
+            departmentName = "Electronics";
+        else if (d == 'H')
+            departmentName = "Hardware";
+        else if (d == 'S')
+            departmentName = "Sportswear";
+        else
+            departmentName = "Unknown";
+
+    }
+
+    public void setSales(double s)
+    {
+        if (s < 2000)
+            sales = 2000;
+        else 
+            sales = s;
+        
+    }
+
+    public double calcSalary()
+    {
+        double basicSalary = 3000;
+        double commission = 0;
+
+        if (departmentName == "Hardware")
+            commission = sales * 0.02;
+        else if (departmentName == "Electronics")
+            commission = sales * 0.035;
+        else if (departmentName == "Sportswear")
+            commission = sales * 0.045;
+        return basicSalary + commission;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public string getSurname()
+    {
+        return surname;
+    }
+    public string getDepartmentName()
+    {
+        return departmentName;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Employee[]  employees = new Employee[6];
+
+        for (int i = 0; i < employees.Length; i++)
+        {
+            employees[i] = new Employee();
+
+            Console.WriteLine("Enter employee" + (i + 1) + "details");
+
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            employees[i].setName(name);
+
+            Console.WriteLine("Enter Surname");
+            string surname = Console.ReadLine();
+            employees[i].setSurname(surname);
+
+            Console.WriteLine("Enter department (E/H/S): ");
+            char dep = Convert.ToChar(Console.ReadLine());
+            employees[i].setDepartmentName(dep);
+
+            Console.WriteLine("Enter monthly sales: ");
+            double sales = Convert.ToDouble(Console.ReadLine());
+            employees[i].setSales(sales);
+
+            Console.WriteLine();
+
+
+        }
+        Console.WriteLine("\nEmployee information\n");
+
+        for (int i = 0; i < employees.Length; i++)
+        {
+            Console.WriteLine("Employee" + (i + 1) + "details");
+            Console.WriteLine("Name: " + employees[i].getName());
+            Console.WriteLine("Surname: " + employees[i].getSurname());
+            Console.WriteLine("Department: " + employees[i].getDepartmentName());
+            Console.WriteLine("Salary: " + employees[i].calcSalary());
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
